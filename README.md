4075586fe9b9e63575ba6fb1175fee899f253053
itssharmajee <sgautamkumar71@gmail.com>
2025-03-10 13:50:24 +0530

    added <Link to=""> Components that navigate to the specific URL

---
+++ README.md
@@ -0,0 +1,13 @@
+# react with Parcel 
+
+## important topics to create a notes...
+- Normal JS utility function like const name = 'sapna';
+- React Local state variable useState() 
+eg: const [name, setName] = useState('sapna');
+
+- useEffect() 
+
+### Normally types of Routing 
+- client-side routing : it just replace previous component with current about page. So it does not make a server request and making a SPA efficient and faster 
+- server-side routing : in this as we chick on achor tag it makes a sever request for particular page like about and returen back to client as rendered 
+
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { LOGO_URL } from "../utils/constraints";
+import { Link } from "react-router-dom";
 
 export const Header = () => {
   const [toggle, setToggle] = useState(false);
@@ -13,10 +14,13 @@ export const Header = () => {
         <img src={LOGO_URL} alt="Company Logo" />
       </div>
       <nav className="nav-links">
-        <a href="#">Home</a>
-        <a href="#">About</a>
-        <a href="#">Services</a>
-        <a href="#">Contact</a>
+        {/* <p><a href="/">Home</a></p> As you can navigate in this way as well but you should not use this because it completely refresh/ reload the entire page so */}
+        <Link to='/'>Home </Link>{/* so we use Link component of react router dom that does not refresh completely the page*/}
+
+        <Link to='/about'>About </Link>
+        <Link to='/service'>Services </Link>
+        <Link to='/contact'>Contact </Link>
+
         <button className="btn" onClick={loginToggle}>
           {toggle ? "Login In" : "Log Out"}
         </button>
